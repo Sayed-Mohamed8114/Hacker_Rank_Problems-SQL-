48fafbd5ca9c9569f42eacb43d82aff2e70d41d2
Sayed-Mohamed8114 <sayed.mohamed8114@gmail.com>
2025-10-20 00:53:50 +0300

    return the largest and smallest city by their length of letters

---
+++ weather_observation_station_5.sql
@@ -0,0 +1,13 @@
+(
+    select city , length(city) as name_length
+    from station 
+    order by length(city) asc , city asc 
+    limit 1
+)
+UNION 
+(
+    select city , length(city) as name_length 
+    from station 
+    order by length(city) desc , city asc 
+    limit 1
+);
